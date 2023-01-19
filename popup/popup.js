6c08f22059210cc9b7ca222431e77b722dcbfcd2
Gabriel Barrientos <gabri12.rubik@gmail.com>
2023-01-19 19:40:40 -0300

    Cambiar el nombre según el estado del checkbox

--- popup/popup.js
+++ popup/popup.js
@@ -1,1 +1,6 @@
-console.log('Hola mundo desde js')
+const modo = document.querySelector('#modo')
+const texto_modo = document.querySelector('#texto_modo')
+
+modo.addEventListener('input', (e) => {
+    texto_modo.innerHTML = e.target.checked ? 'Modo J' : 'Modo P'
+})
