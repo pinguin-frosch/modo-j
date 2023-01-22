0937bda3c6798afed66e43e4d52ca69606d2dca2
Gabriel Barrientos <gabri12.rubik@gmail.com>
2023-01-21 22:22:16 -0300

    Actualizar los valores apenas cambien

--- background.js
+++ background.js
@@ -5,3 +5,31 @@ chrome.runtime.onInstalled.addListener(() => {
         'sitios': []
     })
 })
+
+// Valores por defecto
+let sitios = []
+let activo = false
+
+// Leer los sitios
+chrome.storage.sync.get('sitios', (data) => {
+    sitios = data.sitios
+})
+
+// Leer el estado
+chrome.storage.sync.get('activo', (data) => {
+    activo = data.activo
+})
+
+// Actualizar los valores cuando cambien
+chrome.storage.onChanged.addListener((changes, namespace) => {
+    if (namespace === 'sync') {
+        if (changes.sitios) {
+            sitios = changes.sitios.newValue
+        }
+        if (changes.activo) {
+            activo = changes.activo.newValue
+        }
+    }
+    console.log('sitios:', sitios)
+    console.log('activo:', activo)
+})
